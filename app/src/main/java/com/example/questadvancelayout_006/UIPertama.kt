5a899c6c0fc3b6c9a0f27f301748b7fd29505624
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-10-24 16:10:11 +0700

    merubah warna container menjadi blue

--- app/src/main/java/com/example/questadvancelayout_006/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_006/UIPertama.kt
@@ -52,7 +52,7 @@ fun ActivitasPertama(modifier: Modifier) {
                 .fillMaxWidth(fraction = 1f)
                 .padding(all = 12.dp),
             colors = CardDefaults.cardColors(
-                containerColor = Color.DarkGray
+                containerColor = Color.Blue
             )
         ) {
             Row (
