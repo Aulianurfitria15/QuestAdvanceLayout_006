d3f604ca75fe1caabd7e0d7f9a6f852b97d192b8
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-10-24 15:59:38 +0700

    menambahkan row yg berisi logo umy dengan padding dan jga ukurannya

--- app/src/main/java/com/example/questadvancelayout_006/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_006/UIPertama.kt
@@ -54,5 +54,20 @@ fun ActivitasPertama(modifier: Modifier) {
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )
-        )
+        ) {
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(all = 10.dp)
+            ){
+                val gambar = painterResource(id = R.drawable.logo_umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
+                )
+
+
+    }
 }
