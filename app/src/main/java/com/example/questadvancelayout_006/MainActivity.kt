733d59d4dae20103fb771ebaf4cdf27bd90d520b
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-10-25 13:34:16 +0700

    menggantii greeting menjadi Activity3 agar terpanggil

--- app/src/main/java/com/example/questadvancelayout_006/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_006/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_006Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ActivitasPertama(
+                    Activity3(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
